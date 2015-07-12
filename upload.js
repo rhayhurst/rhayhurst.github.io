970b9b16cc3c03a7408e4db5e48d6c4a01000e32
plueondee@gmail.com <plueondee@gmail.com>
2015-07-12 14:38:53 -0500

    real added secret

--- upload.js
+++ upload.js
@@ -127,11 +127,15 @@
                 var fd = new FormData();
                 
                 fd.append("image_zip_archive", file);
-                fd.append("Authorization", "IBEIS:N37Z3rzJY53IjcuHHNZQK9KqqXs=");
                 var xhr = new XMLHttpRequest();
                 var output = document.getElementById("output");
-
+                
                 xhr.open("POST", "http://131.193.42.62:5005/api/image/");
+                xhr.setRequestHeader("Authorization","IBEIS:N37Z3rzJY53IjcuHHNZQK9KqqXs=");
+                xml.setRequestHeader("Authorization", "Basic " + btoa('ibeis'+ ":" + 'ibeis'));
+                xhr.setRequestHeader("secret","CB73808F-A6F6-094B-5FCD-385EBAFF8FC0");
+                xhr.setRequestHeader("crossDomain",true);
+                xhr.setRequestHeader("dataType",'jsonp');
                 xhr.onload = function() {
 
                     if(this.status==400){
