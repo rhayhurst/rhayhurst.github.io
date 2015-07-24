428b935db588008aca5461fc4cb968c4c2323814
plueondee@gmail.com <plueondee@gmail.com>
2015-07-24 00:21:25 -0500

    added cancel and upload button and cancel function

--- app.js
+++ app.js
@@ -12,14 +12,14 @@ app.factory('photosFactory', function($http) {
  return{
     getPhotos : function(id) {
         return $http({
-            url: 'http://131.193.42.62:5005/api/image/'+id,
+            url: 'http://131.193.42.62:5005/api/annot/'+id,
             dataType:'jsonp',
             method: 'GET'
         })
     },
     getPath :function() {
         return $http({
-            url: 'http://131.193.42.62:5005/api/image/',
+            url: 'http://131.193.42.62:5005/api/annot/',
             dataType:'jsonp',
             method: 'GET'
         })
@@ -44,6 +44,7 @@ app.factory('photosFactory', function($http) {
       
       $scope.imageStuff = function () 
       {
+         $('#map').hide();
         $scope.imgs =[];
         
        angular.forEach($scope.arrImages, function (items) {
@@ -63,15 +64,54 @@ app.factory('photosFactory', function($http) {
 
        photosFactory.getLon(arr).success(function(data){
             alert(data.response)   
+            
       });
      }
-    
-
-   
      
     $scope.hideStuff = function(items){
       $('#'+items+'').toggle('slow');
     }
+
+    $scope.createMap = function (){
+      $('#pictures').hide();
+      $('#map').show();
+
+
+       var locations = [
+      ['Bondi Beach', -33.890542, 151.274856, 4],
+      ['Coogee Beach', -33.923036, 151.259052, 5],
+      ['Cronulla Beach', -34.028249, 151.157507, 3],
+      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+      ['Maroubra Beach', -33.950198, 151.259302, 1]
+    ];
+
+    var map = new google.maps.Map(document.getElementById('map'), {
+      zoom: 10,
+      center: new google.maps.LatLng(-33.92, 151.25),
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    });
+
+    var infowindow = new google.maps.InfoWindow();
+
+    var marker, i;
+
+    for (i = 0; i < locations.length; i++) {  
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+        map: map
+      });
+
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0]);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+     }
+    }
+
+
+
 
       // var requestIamge = {
       //     url: "http://131.193.42.62:5005/api/image/" + 2,
@@ -89,40 +129,40 @@ app.factory('photosFactory', function($http) {
     
 
 
-    $scope.openInfo = function()
-    {
-    	var image = new Image();
-     	image.onload = function()
-        {
-        	EXIF.getData(image, function() 
-        	{
-        		alert(EXIF.pretty(this));
-            });
-        };
-        image.src = document.getElementById("$index").src;
-    };
+ //    $scope.openInfo = function()
+ //    {
+ //    	var image = new Image();
+ //     	image.onload = function()
+ //        {
+ //        	EXIF.getData(image, function() 
+ //        	{
+ //        		alert(EXIF.pretty(this));
+ //            });
+ //        };
+ //        image.src = document.getElementById("$index").src;
+ //    };
    
-  	$scope.inputInfo = function()
-  	{
-  		document.getElementById("file-input").onchange = function(e) 
-  		{
-  			EXIF.getData(e.target.files[0], function() 
-  			{
-  				alert(EXIF.pretty(this));
-        	});
-    	}
-	};
+ //  	$scope.inputInfo = function()
+ //  	{
+ //  		document.getElementById("file-input").onchange = function(e) 
+ //  		{
+ //  			EXIF.getData(e.target.files[0], function() 
+ //  			{
+ //  				alert(EXIF.pretty(this));
+ //        	});
+ //    	}
+	// };
 
-	$scope.Imgfunc = function()
-	{
-		var image = new Image();
-        image.onload = function() 
-        {
-        	EXIF.getData(image, function() 
-        	{
-                alert(EXIF.pretty(this));
-            });
-        };
-        image.src = $scope.image;
-      }
+	// $scope.Imgfunc = function()
+	// {
+	// 	var image = new Image();
+ //        image.onload = function() 
+ //        {
+ //        	EXIF.getData(image, function() 
+ //        	{
+ //                alert(EXIF.pretty(this));
+ //            });
+ //        };
+ //        image.src = $scope.image;
+ //      }
 });
